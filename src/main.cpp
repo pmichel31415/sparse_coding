c0e557c00926f9b1fdf9e20a309de5f91feff27a
AntoinePrv <antoineprouvost@condor.home>
2015-12-11 21:22:17 +0100

    Macros

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include "CImg.h"
 #include <time.h>
+#include <iostream>
+
+using namespace std;
+
 using namespace cimg_library;
 typedef unsigned char uchar;
 
+#define ANTOINE
+
+#ifdef ANTOINE
+#define FOLDER "/Users/antoineprouvost/GitHub/sparse_coding/images/"
+#endif
+
+#ifdef PAUL
+#define FOLDER "../images/"
+#endif
+
+#define FILE "donuts.bmp"
+
+
 int main(){
-	CImg<uchar> image("./donuts.bmp");
+
+	CImg<uchar> image(FOLDER FILE);
 	CImg<uchar> rot(image);
 	uchar * ptr;
 
@@ -15,7 +33,8 @@ int main(){
 	while (!main_disp.is_closed() && !draw_disp.is_closed()) {
 		cimg_for(rot, ptr, uchar){ *ptr = (*ptr + 1) % 256; }
 		draw_disp.display(rot);
-		sleep(20);
+//		sleep(20);
 	}
+
 	return 0;
 }
